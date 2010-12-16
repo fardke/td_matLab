58628d7d167d97a69bde83d3e3a2aea17f447c8c
Kévin Fardel <kevin.fardel@gmail.com>
2010-12-16 17:39:43 +0100

    Ex5 question 1 début.

--- ex5.m
+++ ex5.m
@@ -4,26 +4,43 @@
 clear ;
 clf ;
 
-
-
-
 % Fonction sinus
 
-f0 = 1;		%fréquence du signal
-fplot = 1000 ; % fréquence d'échantillonnage
+f0 = 1000;		%fréquence du signal
+fplot = 10000 ; % fréquence d'échantillonnage
 
 T0 = 1./f0 ;
 
 dt_plot = 1./fplot ;
 
 tmin = 0 ;
-tmax = 4*T0 ;
+tmax = 10*T0 ;
 
 t = tmin:dt_plot:tmax ;
-x = sin (2*pi*f0*t)/2 ;
-X = fft (x) ;
+N = length(t) ;
+x = (sin (2*pi*f0*t)/2) ;
+%X = fft (x) ;
 plot (t, x) ;
+input('Afficher la sinusoide en appliquant le bruit');
+moy = 0;
+ampl = 0.4;
+xB = x.+(moy+ampl*randn(1,N));
+plot(t,xB);
+input('Afficher la transformé de fourier de la sinusoide bruité');
+clf;
+hold on;
+xBF = fft(xB);
+xF = fft(x);
+plot(t,xF,'r');
+plot(t,xBF);
+hold off;
 
-% Choix des unités => amplitude A = 0.4 et période T = 1
-A = 0.4 ;
-T = 1 ;
+input('Application du filtre passe bande');
+f1 = 0.4;
+f2 = 0.1;
+fe = 1;
+bw(1) = 2*f2/fe ;
+bw(2) = 2*f1/fe ;
+h4 = fir1 (N,bw,'blackman');
+H4 = freqz(h4);
+plot(t, abs(H4));
